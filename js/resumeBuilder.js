5de5558cdb2342a30b45535c1431a7134df119e2
Charles <charles.conley@gmail.com>
2016-01-20 16:42:56 -0500

    Added Name and Role

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,21 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
- $("#main").append("Charles Conley");
+$("#main").append("Charles Conley");
+
+var Awesomethought = "I am awesome";
+
+var funthoughts = 
+	Awesomethought.replace("awesome", "fun");
+
+console.log = (Awesomethought);
+
+$("#main").append(funthoughts);
+
+FormatedName = HTMLheaderName.replace("%data%", "Charles Conley");
+
+$("#header").append(FormatedName);
+
+FormatedRole = HTMLheaderRole.replace("%data", "IT Manager")
+
+$("#header").append(FormatedRole);
