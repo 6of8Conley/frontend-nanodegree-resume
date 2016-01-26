37c7f26465adf0eaec8358a3417bfa881c1fe6b0
Charles <charles.conley@gmail.com>
2016-01-26 14:24:43 -0500

    added object and array

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,16 +1,6 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
-$("#main").append("Charles Conley");
-
-var Awesomethought = "I am awesome";
-
-var funthoughts = 
-	Awesomethought.replace("awesome", "fun");
-
-console.log = (Awesomethought);
-
-$("#main").append(funthoughts);
 
 var name = "Charles Conley";
 var role = "IT Manager";
@@ -21,3 +11,38 @@ FormatedRole = HTMLheaderRole.replace("%data%", role);
 
 $("#header").prepend(FormatedRole);
 $("#header").prepend(FormatedName);
+
+var skills = ["Running ", "Fishing ", "Typing "];
+
+var bio = {
+		"name": "Charles Conley ", 
+		"role": "Operations Manager ", 
+		"contactinfo": {
+			"email" : "charles.conley@gmail.com ",
+			"phone" : "404-808-5421 "
+			}, 
+		"picURL": "images/me.png ",
+		"welcomeMsg" : "Hello World ",
+		"skills" : skills 
+	};
+
+bio.city = "Atlanta";
+
+$("#main").append(bio.name);
+$("#main").append(bio.role);
+$("#main").append(bio.contactinfo.phone);
+$("#main").append(bio.picURL);
+$("#main").append(bio.skills);
+$("#main").append(bio.welcomeMsg);
+$("#main").append(bio.city);
+
+var work ={}
+work.current = "Cox Automotive ";
+work.title = "Operations Manager ";
+work.years = 5;
+work.ciry = "Atlanta ";
+
+
+
+
+$("#main").append(work.current);
